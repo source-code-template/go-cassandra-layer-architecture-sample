2b02206362255e32f80bce45c2ea22f9d19e119b
Duc Nguyen <minhduc140583@gmail.com>
2023-07-16 23:36:31 +0700

    Apply new version

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/core-go/core v0.3.5
 	github.com/core-go/health v0.4.9
 	github.com/core-go/log v0.1.4
-	github.com/core-go/search v0.2.11
+	github.com/core-go/search v0.2.12
 	github.com/go-playground/validator/v10 v10.14.1 // indirect
 	github.com/gocql/gocql v1.4.0
 	github.com/gorilla/mux v1.8.0
--- internal/service/user_usecase.go
+++ internal/service/user_usecase.go
@@ -7,32 +7,32 @@ import (
 	. "go-service/internal/repository"
 )
 
-func NewUserService(repository UserRepository) *UserUsecase {
-	return &UserUsecase{repository: repository}
+func NewUserService(repository UserRepository) *UserUseCase {
+	return &UserUseCase{repository: repository}
 }
 
-type UserUsecase struct {
+type UserUseCase struct {
 	repository UserRepository
 }
 
-func (s *UserUsecase) All(ctx context.Context) ([]User, error) {
+func (s *UserUseCase) All(ctx context.Context) ([]User, error) {
 	return s.repository.All(ctx)
 }
-func (s *UserUsecase) Load(ctx context.Context, id string) (*User, error) {
+func (s *UserUseCase) Load(ctx context.Context, id string) (*User, error) {
 	return s.repository.Load(ctx, id)
 }
-func (s *UserUsecase) Create(ctx context.Context, user *User) (int64, error) {
+func (s *UserUseCase) Create(ctx context.Context, user *User) (int64, error) {
 	return s.repository.Create(ctx, user)
 }
-func (s *UserUsecase) Update(ctx context.Context, user *User) (int64, error) {
+func (s *UserUseCase) Update(ctx context.Context, user *User) (int64, error) {
 	return s.repository.Update(ctx, user)
 }
-func (s *UserUsecase) Patch(ctx context.Context, user map[string]interface{}) (int64, error) {
+func (s *UserUseCase) Patch(ctx context.Context, user map[string]interface{}) (int64, error) {
 	return s.repository.Patch(ctx, user)
 }
-func (s *UserUsecase) Delete(ctx context.Context, id string) (int64, error) {
+func (s *UserUseCase) Delete(ctx context.Context, id string) (int64, error) {
 	return s.repository.Delete(ctx, id)
 }
-func (s *UserUsecase) Search(ctx context.Context, filter *UserFilter) ([]User, string, error) {
+func (s *UserUseCase) Search(ctx context.Context, filter *UserFilter) ([]User, string, error) {
 	return s.repository.Search(ctx, filter)
 }
